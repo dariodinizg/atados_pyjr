22e644d1bc56eb9152a436d7833c64ce89fecaba
Dario Diniz <dariodg@gmail.com>
2020-02-07 10:27:31 -0300

    Edited docstring with an example

--- calculator.py
+++ calculator.py
@@ -2,6 +2,18 @@ def stock_calculator(stock_prices: list) -> float:
     """ 
     Calculates what could be the best profit for a trade operation 
     based on a list with stock prices of a given period.
+
+    Ex:
+    import time
+    import pandas as pd
+
+    df = pd.read_csv('itsa4_historica.csv')
+    t0 = time.time()
+    best_profit = stock_calculator(df['FECHAMENTO'])
+    t1 = time.time()
+    print(f'Best selling profit: R${best_profit:.2f}')
+    print(f'Execution time: {t1 - t0}')
+
     """
     profit = 0
     for index, quote in enumerate(stock_prices):
@@ -11,15 +23,3 @@ def stock_calculator(stock_prices: list) -> float:
             if comparison > profit:
                 profit = comparison
     return profit
-
-"""
-import time
-import pandas as pd
-
-df = pd.read_csv('itsa4_historica.csv')
-t0 = time.time()
-best_profit = stock_calculator(df['FECHAMENTO'])
-t1 = time.time()
-print(f'Best selling profit: R${best_profit:.2f}')
-print(f'Execution time: {t1 - t0}')
-"""
